44907d9f9b1ae05b7d965a1f9b0c7884f82c9377
unknown <spprince007@gmail.com>
2022-08-07 18:21:48 +0600

    module 21-3 done

---
+++ split-joint.js
@@ -0,0 +1,23 @@
+    const lyrics="Tumi Bondhu Kala Pakhi ami jeno ki, bosonto kal tomai bolte parini, kala kala sada sada mone ";
+    console.log(lyrics);
+    const parts= lyrics.split(' ');
+    console.log(parts);
+    const sentanse=lyrics.split(",");
+    console.log(sentanse);
+    const chract=lyrics.split('');
+    console.log(chract);
+    const partial= lyrics.slice(5,11);
+    console.log(partial);
+    const partial1= lyrics.substring(5,11);
+    console.log(partial1);
+    console.log(lyrics.toUpperCase());
+    console.log(lyrics.toLocaleLowerCase());
+    const lines=[
+        'Tumi bondhu kala pakhi',
+        'imi jeno ki',
+        'bosonto kale tomay bolte parini',
+        'sada sada kala kala rong jomece sada kala'
+    ]
+    console.log(lines.join('. '));
+    // console.log(lines.toUpperCase());
+    
