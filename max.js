f79db13d3e81dd8813fb75002c7c2865728065bf
unknown <spprince007@gmail.com>
2022-08-08 19:10:59 +0600

    js test of max

---
+++ max.js
@@ -0,0 +1,7 @@
+const jim = 84;
+const dela= 97;
+if(jim>dela){
+    console.log('jim will get the cake');
+}else{
+    console.log('dela will get the cake');
+}
---
+++ max3.js
@@ -0,0 +1,13 @@
+const jim=30;
+const dela=89;
+const jery=97;
+
+if(jim>dela && jim>jery){
+    console.log('jim will get the cake');
+}
+else if(dela>jim && dela>jery){
+    console.log('dela will get the cake');
+}
+else{
+    console.log('jery will get wthe cake');
+}
